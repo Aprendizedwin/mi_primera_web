d505b6645bcbf3f18d50dc755c89c99da11556c9
Aprendizedwin <edwin.perafan@hotmail.com>
2023-10-08 19:24:19 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//alert("Hola este es mi Javascript");
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = function () {
+  navBar = document.querySelector(".navbar");
+  navBar.classList.toggle("active");
+};
